b998e6f828c0c9c1443b39da51533270eb3e98c5
damigas <63496571+damigas@users.noreply.github.com>
2020-04-19 22:47:02 +0200

    Dami Upload

--- views/addCocktail.js
+++ views/addCocktail.js
@@ -3,21 +3,27 @@ var myObj;
 $(document).ready(function(){
     $("#select-btn").prop("disabled",true);
     $("#add-btn").prop("disabled",true);
+    $('#right-column').hide();
 });
 
 function SearchByName(){
+    document.getElementById("search_cocktail_name_msg").innerHTML = "";
     var search_cocktail_name =  document.getElementById('search_cocktail_name').value;
     var request = new XMLHttpRequest();
-    request.open('GET', 'https://pacific-stream-14038.herokuapp.com/cocktail/'+search_cocktail_name, true);
+    request.open('GET', `https://pacific-stream-14038.herokuapp.com/cocktail/${search_cocktail_name}/multiple/10`, true);
     request.onload = function() {
     // Begin accessing JSON data here
         if (request.status >= 200 && request.status < 400){
+            document.getElementById("menu_cocktail").innerHTML="";
             myObj = JSON.parse(request.response);
-            document.getElementById("menu_cocktail").innerHTML =  `<option value=${myObj.name}> ${myObj.name}</option>`;    
+            for(i=0; i< myObj.length; i++){
+                document.getElementById("menu_cocktail").innerHTML +=  `<option value=${myObj[i].name}> ${myObj[i].name}</option>`;
+            }
+            //document.getElementById("menu_cocktail").innerHTML =  `<option value=${myObj.name}> ${myObj.name}</option>`;    
             $("#select-btn").prop("disabled",false);  
         }  
         else{
-            alert("Error connecting");
+            document.getElementById("search_cocktail_name_msg").innerHTML = "Cocktail not found! Please try again";
         }
     }
     request.send();
@@ -29,24 +35,34 @@ function SelectCocktail(){
     if(item_selected.text==""){
         alert("Please select a cocktail in the menu!")
     }
-    else{        
-       // var arr = myObj.drinks;
-       // var i, result;
+    else{
         document.getElementById("description_list").innerHTML="";
-        //for(i=0; i< arr.length; i++){
-           // result=arr[i];                
-            /*if(result.strDrink==item_selected.text){
-                document.getElementById("photo_cocktail").src= result.strDrinkThumb;
-                document.getElementById("recipe").innerHTML= result.strInstructions;                
-                document.getElementById("description_list").innerHTML+= `<dt>Category</dt> <dd>- ${result.strCategory}</dd>`;
-                document.getElementById("description_list").innerHTML+= `<dt>Type</dt> <dd>- ${result.strAlcoholic}</dd>`;
+        var j;
+        for(j=0; j< myObj.length; j++){               
+            if(myObj[j].name==item_selected.text){
+                document.getElementById("photo_cocktail").src= myObj[j].image;
+                document.getElementById("recipe").innerHTML= myObj[j].instructions;                
+                document.getElementById("description_list").innerHTML+= `<dt>Category</dt> <dd>- ${myObj[j].category}</dd>`;
+                document.getElementById("description_list").innerHTML+= `<dt>Composition:</dt> <dd><table id="tab" ><tr><th>Ingredients</th><th>Quantities</th></tr> </table></dd>`;
+                var ing = myObj[j].ingredients;
+                var qnt = myObj[j].quantities;
+                var i;
+                for(i=0; i<ing.length; i++){
+                    if(typeof qnt[i] !== "undefined"){
+                        document.getElementById("tab").innerHTML += `<tr><td>${ing[i]}</td><td>${qnt[i]}</td></tr>`;
+                    }
+                    else{
+                        document.getElementById("tab").innerHTML += `<tr><td>${ing[i]}</td><td>not specified</td></tr>`;
+                    }
+                }
             }
-        }*/
-        if(myObj.name==item_selected.text){
+        }
+        $('#right-column').show(100);
+        /*if(myObj.name==item_selected.text){
             document.getElementById("photo_cocktail").src= myObj.image;
             document.getElementById("recipe").innerHTML= myObj.instructions;                
             document.getElementById("description_list").innerHTML+= `<dt>Category</dt> <dd>- ${myObj.category}</dd>`;
-            document.getElementById("description_list").innerHTML+= `<dt>Composition:</dt> <dd><table id="tab" width=30%> </table></dd>`;
+            document.getElementById("description_list").innerHTML+= `<dt>Composition:</dt> <dd><table id="tab" width=39%> </table></dd>`;
             var ing = myObj.ingredients;
             var qnt = myObj.quantities;
             var i;
@@ -54,7 +70,75 @@ function SelectCocktail(){
                 if(typeof qnt[i] !== "undefined"){
                     document.getElementById("tab").innerHTML += `<tr><td>${ing[i]}</td><td>${qnt[i]}</td></tr>`;
                 }
+                else{
+                    document.getElementById("tab").innerHTML += `<tr><td>${ing[i]}</td><td>not specified</td></tr>`;
+                }
+            }
+        }*/
+    }
+}
+
+function RandomCocktail(){
+    var request = new XMLHttpRequest();
+    request.open('GET', `https://pacific-stream-14038.herokuapp.com/cocktail/surprise/10`, true);
+    request.onload = function() {
+    // Begin accessing JSON data here
+        if (request.status >= 200 && request.status < 400){
+            document.getElementById("menu_cocktail").innerHTML="";
+            myObj = JSON.parse(request.response);
+            for(i=0; i< myObj.length; i++){
+                document.getElementById("menu_cocktail").innerHTML +=  `<option value=${myObj[i].name}> ${myObj[i].name}</option>`;
+            }
+            //document.getElementById("menu_cocktail").innerHTML =  `<option value=${myObj.name}> ${myObj.name}</option>`;    
+            $("#select-btn").prop("disabled",false);  
+        }  
+    }
+    request.send();
+}
+
+function RadioCocktail(){
+    document.getElementById("search_cocktail_radio_msg").innerHTML="";
+    if(document.getElementById('ingredients').checked){
+        var search_cocktail_ingredient =  document.getElementById('search_cocktail_radio').value;
+        var request = new XMLHttpRequest();
+        request.open('GET', `https://pacific-stream-14038.herokuapp.com/cocktail/ingredient/10/${search_cocktail_ingredient}`, true);
+        request.onload = function() {
+        // Begin accessing JSON data here
+            if (request.status >= 200 && request.status < 400){
+                document.getElementById("menu_cocktail").innerHTML="";
+                myObj = JSON.parse(request.response);
+                for(i=0; i< myObj.length; i++){
+                    document.getElementById("menu_cocktail").innerHTML +=  `<option value=${myObj[i].name}> ${myObj[i].name}</option>`;
+                }  
+                $("#select-btn").prop("disabled",false);  
+            }  
+            else{
+                document.getElementById("search_cocktail_radio_msg").innerHTML = "Ingredient not found! Please try again";
             }
         }
+        request.send();
+    }      
+    else if(document.getElementById('category').checked){
+        var search_cocktail_category =  document.getElementById('search_cocktail_radio').value;
+        var request = new XMLHttpRequest();
+        request.open('GET', `https://pacific-stream-14038.herokuapp.com/cocktail/byCategory/${search_cocktail_category}/10`, true);
+        request.onload = function() {
+        // Begin accessing JSON data here
+            if (request.status >= 200 && request.status < 400){
+                document.getElementById("menu_cocktail").innerHTML="";
+                myObj = JSON.parse(request.response);
+                for(i=0; i< myObj.length; i++){
+                    document.getElementById("menu_cocktail").innerHTML +=  `<option value=${myObj[i].name}> ${myObj[i].name}</option>`;
+                }  
+                $("#select-btn").prop("disabled",false);  
+            }  
+            else{
+                document.getElementById("search_cocktail_radio_msg").innerHTML = "Category not found! Please try again";
+            }
+        }
+        request.send();
+    }      
+    else{
+        document.getElementById("search_cocktail_radio_msg").innerHTML="Please select a selection criteria";
     }
 }
