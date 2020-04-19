c6d3f406f7fe1f01bb4ac429d2e30bad2521bac6
anadasca <anamdasca98@gmail.com>
2020-04-19 19:05:04 +0200

    Prima bozza (non completa) di my nights, my saved nights e my favourite nights

--- views/profile.js
+++ views/profile.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
 
     document.getElementById("username_here").innerHTML = username + "'s profile";
     //$("#message").hide();
-    
+    document.getElementById('my_night').style.color = '#CC2A41';
+    document.getElementById('message').innerHTML = 'Your nights will appear here!Start creating now';
+
     //Funzione che carica le notti dinamicamente
     function findNights(){
      
@@ -16,11 +18,13 @@ $(document).ready(function(){
       if (request.status >= 200 && request.status < 400) {
           var risposta = JSON.parse(this.response);
           if(risposta.length == 0){
+            document.getElementById('message').innerHTML = 'Your nights will appear here! Start creating now!';
             $("#message").show();
 
           }
           else{
-            //alert("Ci sono delle notti!")
+            document.getElementById('message').innerHTML = 'Your nights will appear here! Start creating now!';
+            alert("Ci sono delle notti!Codice da implementare")
 
           }
           }
@@ -30,19 +34,97 @@ $(document).ready(function(){
           } 
       }
       request.send();   
+    }
+    
 
+    //funzione che carica le notti salvate dinamicamente
+    function findSavedNights(){
+     
+      var request = new XMLHttpRequest();
+      var path =  'https://pacific-stream-14038.herokuapp.com/saved/'+username ;
+      request.open('GET', path, true)
+      request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+          var risposta = JSON.parse(this.response);
+          if(risposta.length == 0){
+            document.getElementById('message').innerHTML = 'Your saved nights will appear here! Browse some night on the feed page!';
+            $("#message").show();
+          }
+          else{
+            alert("Ci sono delle notti!Codice da implementare")
 
+          }
+      }
+
+      else {
+          alert("Something went wrong");
+          } 
+      }
+      request.send();   
+    }
+    
+
+    //funzione che carica le notti preferite dinamicamente
+    function findFavouriteNights(){
+     
+      var request = new XMLHttpRequest();
+      var path =  'https://pacific-stream-14038.herokuapp.com/upvoted/'+username ;
+      request.open('GET', path, true)
+      request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+          var risposta = JSON.parse(this.response);
+          if(risposta.length == 0){
+            document.getElementById('message').innerHTML = 'Your favourites nights will appear here! Browse some night on the feed page!';
+            $("#message").show();
+          }
+          else{
+            alert("Ci sono delle notti! Codice da implementare")
+
+          }
+      }
+
+      else {
+          alert("Something went wrong");
+          } 
+      }
+      request.send();   
     }
 
     findNights();
+
+    $("#my_night").click(function(){
+
+      document.getElementById('my_night').style.color = '#CC2A41';
+      document.getElementById('my_saved').style.color = '#424254';
+      document.getElementById('my_favourite').style.color ='#424254';
+      findNights();
+    });
+
+    //my_saved
+    $("#my_saved").click(function(){
+
+      document.getElementById('my_saved').style.color = '#CC2A41';
+      document.getElementById('my_night').style.color = '#424254';
+      document.getElementById('my_favourite').style.color ='#424254';
+      findSavedNights();
+    });
    
-  });
+    //my_favourites
+    $("#my_favourite").click( function(){
+
+      document.getElementById('my_favourite').style.color = '#CC2A41';
+      document.getElementById('my_night').style.color = '#424254';
+      document.getElementById('my_saved').style.color = '#424254';
+      findFavouriteNights();
+    })
+
+});
   
 function logout(){
   var result = confirm("Are you sure you want to logout?");
     if (result) {
       alert("You are logging out! Bye!");
       window.location.href = "./home.html";  
-}
+    }
 }
 
