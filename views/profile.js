8837a5a5c733dfa73c85045691984f282188f18b
anadasca <anamdasca98@gmail.com>
2020-04-19 18:32:10 +0200

    Prima bozza (non completa) di my nights

--- views/profile.js
+++ views/profile.js
@@ -1,6 +1,40 @@
+var username = localStorage.getItem("username");
+
+
 $(document).ready(function(){
-    // Retrieve
-    document.getElementById("username_here").innerHTML = localStorage.getItem("username") + "'s profile";
+
+    document.getElementById("username_here").innerHTML = username + "'s profile";
+    //$("#message").hide();
+    
+    //Funzione che carica le notti dinamicamente
+    function findNights(){
+     
+      var request = new XMLHttpRequest();
+      var path =  'https://pacific-stream-14038.herokuapp.com/created/'+username ;
+      request.open('GET', path, true)
+      request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+          var risposta = JSON.parse(this.response);
+          if(risposta.length == 0){
+            $("#message").show();
+
+          }
+          else{
+            //alert("Ci sono delle notti!")
+
+          }
+          }
+
+      else {
+          alert("Something went wrong");
+          } 
+      }
+      request.send();   
+
+
+    }
+
+    findNights();
    
   });
   
@@ -11,3 +45,4 @@ function logout(){
       window.location.href = "./home.html";  
 }
 }
+
