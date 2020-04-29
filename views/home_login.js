b7e939cd52d1d0b424d00b51d29bab77c91b43cf
anadasca <anamdasca98@gmail.com>
2020-04-29 11:37:58 +0200

    Stampe tolte

--- views/home_login.js
+++ views/home_login.js
@@ -1,25 +1,23 @@
 function sendData(){
-    alert("Sending data");
+    //alert("Sending data");
     var params = (new URL(document.location)).searchParams;
     var code = params.get('code'); 
    
-    alert("Code: " + code);
+    //alert("Code: " + code);
     
   var request = new XMLHttpRequest();
   var path = 'https://pacific-stream-14038.herokuapp.com/auth/callback?code=' + code ;
-  alert("Path: " + path);
+  //alert("Path: " + path);
   request.open('GET', path, true)
   request.onload = function() {
  
   if (request.status >= 200 && request.status < 400) {
-        var risposta = JSON.parse(this.response);
-        alert("Accesso avvenuto con successo");
-        localStorage.setItem("username", risposta.nickname);
-        alert("Username memorizzato");
-        alert("Nickname: " + JSON.stringify(risposta.nickname));
-        alert("Nickname nello storage: " + localStorage.getItem("username") );
         
+        var risposta = JSON.parse(this.response);
+        localStorage.setItem("username", risposta.nickname);
+        alert("Accesso avvenuto con successo");
        } 
+       
   else {
       alert("Something went wrong!");
       } 
