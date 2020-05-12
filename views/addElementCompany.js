c6f43169ebcb910961de82f473dd055e4982dde4
damigas <63496571+damigas@users.noreply.github.com>
2020-05-12 21:33:57 +0200

    Flip & Alignment

--- views/addElementCompany.js
+++ views/addElementCompany.js
@@ -14,56 +14,51 @@ function CheckStorage(){
         $(".button").prop("disabled",false);
     }
     else $(".button").prop("disabled",true);
-}
-
-$(document).ready(function () {
 
     if(typeof(localStorage.cocktail) != "undefined"){
         var cocktail = JSON.parse(localStorage.cocktail);
-        document.getElementById('cocktail_card').style.backgroundImage = `url("${cocktail.image}")`;
-        document.getElementById('cocktail').innerHTML = `${cocktail.name}`;
-        document.getElementById('change_cocktail').innerHTML = "Change your cocktail!";
+        document.querySelector("#cocktail_card .flip-card-front").style.backgroundImage = `url("${cocktail.image}")`;
+        document.querySelector("#cocktail_card .card-title").innerHTML = `${cocktail.name}`;
+        document.querySelector("#cocktail_card .change-title").innerHTML = "Change your cocktail!";
     }
 
     if(typeof(localStorage.board_game) != "undefined"){
-
         var game = JSON.parse(localStorage.board_game);
-        document.getElementById('game_card').style.backgroundImage = `url("${game.imageUrl}")`;
-        document.getElementById('boardgame').innerHTML = `${game.name}`;
-        document.getElementById('change_boardgame').innerHTML = "Change your boardgame!";
+        document.querySelector("#game_card .flip-card-front").style.backgroundImage = `url("${game.imageUrl}")`;
+        document.querySelector("#game_card .card-title").innerHTML = `${game.name}`;
+        document.querySelector("#game_card .change-title").innerHTML = "Change your boardgame!";
     }
     
+
     if(typeof(localStorage.movie) != "undefined"){
 
         var movie= JSON.parse(localStorage.movie);
-        if(movie.playbill != null) document.getElementById('movie_card').style.backgroundImage = `url("${movie.playbill}")`;
-        document.getElementById('movie').innerHTML = `${movie.title}`;
-        document.getElementById('change_media').innerHTML = "Change your movie or tv show!";
+        if(movie.playbill != null) document.querySelector("#movie_card .flip-card-front").style.backgroundImage = `url("${movie.playbill}")`;
+        document.querySelector("#movie_card .card-title").innerHTML = `${movie.title}`;
+        document.querySelector("#movie_card .change-title").innerHTML = "Change your movie or tv show!";
     } 
 
     if(typeof(localStorage.beer) != "undefined" ){
  
         var beer= JSON.parse(localStorage.beer);
-        if(beer.image != "Sorry, no picture provided for this beer") document.getElementById('beer_card').style.backgroundImage = `url("${beer.image}")`;
-        document.getElementById('beer').innerHTML = `${beer.name}`;
-        document.getElementById('change_beer').innerHTML = "Change your beer!";
+        if(beer.image != "Sorry, no picture provided for this beer") document.querySelector("#beer_card .flip-card-front").style.backgroundImage = `url("${beer.image}")`;
+        document.querySelector("#beer_card .card-title").innerHTML = `${beer.name}`;
+        document.querySelector("#beer_card .change-title").innerHTML = "Change your beer!";
     }
 
     if(typeof(localStorage.meal) != "undefined"){
 
         var meal = JSON.parse(localStorage.meal);
-        document.getElementById('recipe_card').style.backgroundImage = `url("${meal.image}")`;
-        document.getElementById('meal').innerHTML = `${meal.name}`;
-        document.getElementById('change_meal').innerHTML = "Change your meal!";
+        document.querySelector("#recipe_card .flip-card-front").style.backgroundImage = `url("${meal.image}")`;
+        document.querySelector("#recipe_card .card-title").innerHTML = `${meal.name}`;
+        document.querySelector("#recipe_card .change-title").innerHTML = "Change your meal!";
     }
 
     if(typeof(localStorage.artist) != "undefined"){
  
         var music = JSON.parse(localStorage.artist);
-        document.getElementById('music_card').style.backgroundImage = `url("${music.album_image}")`;
-        document.getElementById('music').innerHTML = `${music.name}`;
-        document.getElementById('change_music').innerHTML = "Change your music!";
+        document.querySelector("#music_card .flip-card-front").style.backgroundImage = `url("${music.album_image}")`;
+        document.querySelector("#music_card .card-title").innerHTML = `${music.name}`;
+        document.querySelector("#music_card .change-title").innerHTML = "Change your music!";
     }
-   
-  });
-  
+}
